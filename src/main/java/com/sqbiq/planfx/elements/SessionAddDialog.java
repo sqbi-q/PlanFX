bc63a305441cecc9066fd0649e199af2d5c7dbd3
Sebastian <sebasqbi@gmail.com>
2024-03-04 09:27:00 +0100

    Add fields

--- src/main/java/com/sqbiq/planfx/elements/SessionAddDialog.java
+++ src/main/java/com/sqbiq/planfx/elements/SessionAddDialog.java
@@ -1,9 +1,41 @@
 package com.sqbiq.planfx.elements;
 
-import javafx.scene.control.Dialog;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 public class SessionAddDialog extends Dialog {
     public SessionAddDialog() {
         setTitle("Add Session");
+
+        GridPane fieldsPane = new GridPane();
+
+        TextField title = new TextField();
+        Label titleLabel = new Label("Title:");
+        fieldsPane.add(titleLabel, 0, 0, 2, 1);
+        fieldsPane.add(title, 0, 1, 2, 1);
+
+        TextArea description = new TextArea();
+        Label descriptionLabel = new Label("Description:");
+        fieldsPane.add(descriptionLabel, 0, 2, 2, 1);
+        fieldsPane.add(description, 0, 3, 2, 1);
+
+        DatePicker dateBegin = new DatePicker();
+        Label dateBeginLabel = new Label("Date begin:");
+        fieldsPane.add(dateBeginLabel, 0, 4, 1, 1);
+        fieldsPane.add(dateBegin, 0, 5, 1, 1);
+
+        DatePicker dateEnd = new DatePicker();
+        Label dateEndLabel = new Label("Date end:");
+        fieldsPane.add(dateEndLabel, 1, 4, 1, 1);
+        fieldsPane.add(dateEnd, 1, 5, 1, 1);
+
+        getDialogPane().setContent(fieldsPane);
+
+        ButtonType cancelButtonType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        ButtonType addButtonType = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
+        getDialogPane().getButtonTypes().add(cancelButtonType);
+        getDialogPane().getButtonTypes().add(addButtonType);
+        boolean addDisabled = false;
+        getDialogPane().lookupButton(addButtonType).setDisable(addDisabled);
     }
 }
