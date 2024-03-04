cabe3ce09543706cd05b52c26631e1e05dc1371a
Sebastian <sebasqbi@gmail.com>
2024-03-04 10:02:43 +0100

    Add toolbar

--- src/main/java/com/sqbiq/planfx/HelloApplication.java
+++ src/main/java/com/sqbiq/planfx/HelloApplication.java
@@ -4,6 +4,8 @@ import com.sqbiq.planfx.elements.SessionAddDialog;
 import com.sqbiq.planfx.session.Session;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -13,16 +15,32 @@ import java.util.Optional;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = new Scene(new Pane(), 320, 240);
+        Pane root = new Pane();
+        root.getChildren().add(getApplicationToolbar(stage));
+
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
 
-        SessionAddDialog dialog = new SessionAddDialog();
-        Optional<Session> dialogResult = dialog.showAndWait();
-        dialogResult.ifPresent(session -> {
-            System.out.println(session.getTitle());
+    private ToolBar getApplicationToolbar(Stage stage) {
+        ToolBar toolbar = new ToolBar();
+        toolbar.prefWidthProperty().bind(stage.widthProperty());
+
+        Button addSessionButton = new Button("Add session");
+
+        addSessionButton.setOnAction(event -> {
+            SessionAddDialog dialog = new SessionAddDialog();
+            Optional<Session> dialogResult = dialog.showAndWait();
+            dialogResult.ifPresent(session -> {
+                System.out.println(session.getTitle());
+            });
         });
+
+        toolbar.getItems().add(addSessionButton);
+
+        return toolbar;
     }
 
     public static void main(String[] args) {
