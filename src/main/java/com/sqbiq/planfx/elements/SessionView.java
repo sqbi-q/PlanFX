1e04dc3adf0ef444d6243d9eb9ce4ba4dc6e6d10
Sebastian <sebasqbi@gmail.com>
2024-03-04 11:30:03 +0100

    Add context menu

--- src/main/java/com/sqbiq/planfx/elements/SessionView.java
+++ src/main/java/com/sqbiq/planfx/elements/SessionView.java
@@ -2,7 +2,9 @@ package com.sqbiq.planfx.elements;
 
 import com.sqbiq.planfx.session.Session;
 import javafx.geometry.Insets;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -23,5 +25,27 @@ public class SessionView extends VBox {
         borderProperty().set(new Border(new BorderStroke(Color.BLACK,
                 BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         paddingProperty().set(new Insets(10, 10, 10, 10));
+
+        ContextMenu contextMenu = getContextMenu();
+        setOnContextMenuRequested(e ->
+                contextMenu.show(this, e.getScreenX(), e.getScreenY()));
+    }
+
+    private ContextMenu getContextMenu() {
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem deleteItem = new MenuItem("Delete session");
+        deleteItem.setOnAction(event -> {
+            System.out.println("Delete");
+        });
+        contextMenu.getItems().add(deleteItem);
+
+        MenuItem editItem = new MenuItem("Edit");
+        editItem.setOnAction(event -> {
+            System.out.println("Edit");
+        });
+        contextMenu.getItems().add(editItem);
+
+        return contextMenu;
     }
 }
