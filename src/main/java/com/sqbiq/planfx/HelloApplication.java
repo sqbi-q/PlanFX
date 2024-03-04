86151da974c7da1f2f126a8c5eda2a965f7ddd3a
Sebastian <sebasqbi@gmail.com>
2024-03-04 08:10:34 +0100

    Initial commit

---
+++ src/main/java/com/sqbiq/planfx/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.sqbiq.planfx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new Pane(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.sqbiq.planfx {
+    requires javafx.controls;
+
+    exports com.sqbiq.planfx;
+}
